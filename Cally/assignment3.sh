f77839604e4d5030193d33c3cb5baed0ccf77d0f
cally <callyclaver@gmail.com>
2023-03-08 02:28:48 +0000

    first command-line argumrnt with second-line argument

---
+++ Cally/assignment3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ If statment that compares the first command-line argument with the second command-line argument.
+ Also check if the first line argument is greater than the second line argument.
+ 
+ If [[ $1 -gt $2 ]]
+
+then
