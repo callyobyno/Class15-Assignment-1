55d194e30807a5e0fc6c2ab4d0d11203185761cc
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-08 06:47:21 +0000

    added if..elif..else syntax

--- Aziz/Bash/file-and-directory.sh
+++ Aziz/Bash/file-and-directory.sh
@@ -2,3 +2,18 @@
 
 #user enter name of a file or directory and script tell them if it is file or directory and print out the permission
 
+clear
+read -p " Please enter a name of file or directory in your system: " name
+if [ ]
+then
+	echo
+	ls -la
+elif
+then
+	echo
+	ls -la
+else
+	echo
+	echo
+	sleep 4
+fi
