e3ab725304907a80ca70eb3358504c8ed06f1c72
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-09 22:08:00 +0000

    corrected missing then for if

--- Aziz/Bash/CLI_number_comparison.sh
+++ Aziz/Bash/CLI_number_comparison.sh
@@ -4,19 +4,22 @@
 
 clear
 if [ $# -eq 0 ]
+then
 	echo " you did not enter a number. no comparison can be made."
+	sleep 3
 	echo "                  GOOD BYE "
-elif [  $1 -gt $2 ]
+elif [ $1 -gt $2 ]
 then
 	 echo " the bigger number is :"
-	 sleep 5
+	 sleep 2
 	 echo -e "\e{031m          $1\e[00m "
 elif [ $1 -lt $2 ]
 then
 	echo " The bigger number is below"
-	sleep 5
+	sleep 2
 	echo -e "\e[031m           $2\e[00m" 
 
 else
 	echo -e "\e[031m $1\e[00m is equal to \e[031m$2\e[00m"
 fi
+
