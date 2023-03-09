70efd7d40d9ab95052cf7af4799b3c7394bd1991
Eric Tambe <etambe1@gmail.com>
2023-03-09 00:26:02 +0000

    Second Homework

--- Eric/Eric.sh
+++ Eric/Eric.sh
@@ -15,3 +15,35 @@ fi
 
 
 
+
+# Assignment 2
+
+#!/bin/bash
+
+#This Script is to Create an interactive bash script that takes a filename as argument and analyses it ( Print the permissions to the screen, say if it is a regular file or directory.)
+
+
+#Define Path
+dir=$1
+
+
+#Check if file name is provided
+if [ $# -eq 0 ]
+then
+        echo " Please provide a filename"
+        read -p "path: " dir
+fi
+
+#Check if the file exists
+# ! -e = checks if file exists, "$dir" tell what argument to check
+# ! is to check if false
+if [ ! -e "$dir" ]
+then
+        echo "File does not exist"
+        sleep 1 && echo "Exiting"
+        exit 1
+fi
+
+
+
+
