87db01a2bf877eaca0e054ac769292e8c89182cf
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-09 20:53:41 +0000

    added for loop condition and user choices

--- Aziz/Bash/Calculator.sh
+++ Aziz/Bash/Calculator.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # calculator with a memory
-ans=y
-while 
+ans=Y
+answer=${ans^^}
+while [ $answer == "Y" ]
 do
+	clear
+	clear
+echo " CALCULATOR"
+echo " 1-ADDITION"
+echo " 2-SUBSTRACTION"
+echo " 3-MULTIPLICATION"
+echo " 4-DIVISION"
+read -p "Please choose the  number  that is for the operation you want to perform: " num
+read -p "Enter your firt number : " num1
+read -p "Enter your second number : " num2
+
+read -p "Do you want to continue (Y/N): " ans
+answer=${ans^^}
 done 
