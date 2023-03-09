784c266112aa8e57b44421518e57547bcb32eca6
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-09 21:06:34 +0000

    added if statement wih action to perform

--- Aziz/Bash/Calculator.sh
+++ Aziz/Bash/Calculator.sh
@@ -14,6 +14,19 @@ echo " 4-DIVISION"
 read -p "Please choose the  number  that is for the operation you want to perform: " num
 read -p "Enter your firt number : " num1
 read -p "Enter your second number : " num2
+if  [[ $num -eq 1 ]]
+then
+        echo " $num " + " $num2 " = " $((num1+num2)) "
+elif [[ $num -eq 2 ]]
+then
+        echo " $num1 " - "  $num2 " = " $((num1-num2))"
+elif [[ $num -eq 3 ]]
+then
+        echo " $num1 " X " $num2 " = " $((num1*num2)) "
+elif [[ $num -eq 4 ]]
+then
+        echo "$num1 " / " $num2 " = " $((num1/num2)) "
+fi
 
 read -p "Do you want to continue (Y/N): " ans
 answer=${ans^^}
