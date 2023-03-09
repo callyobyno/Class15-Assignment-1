594b7fe4530ba13d9b4e49837a5f2173acb6c832
Aziz Sawadogo <azizbluestars@gmail.com>
2023-03-09 17:39:34 +0000

    added red color to the print out

--- Aziz/Bash/CLI_number_comparison.sh
+++ Aziz/Bash/CLI_number_comparison.sh
@@ -5,10 +5,15 @@
 clear 
 if [  $1 -gt $2 ]
 then
-	 echo " the bigger number is :";  sleep 5;  echo "  $1 "
+	 echo " the bigger number is :"
+	 sleep 5
+	 echo -e "\e{031m          $1\e[00m "
 elif [ $1 -lt $2 ]
 then
-	echo " The bigger number is $2 "
+	echo " The bigger number is below"
+	sleep 5
+	echo -e "\e[031m           $2\e[00m" 
+
 else
-	echo " $1 is equal to $2 "
+	echo -e "\e[031m $1\e[00m is equal to \e[031m$2\e[00m"
 fi
