a5d80e555588eaa4445c4c6e0b67e30fbc077292
cally <callyclaver@gmail.com>
2023-03-11 11:55:32 +0000

    performance

--- Cally/assignment4.sh
+++ Cally/assignment4.sh
@@ -1,2 +1,3 @@
 Difference between MAVEN and GRADLE
 Configuration: Maven uses XML and it is less expensive while Gradle uses Groovy or Kotlin and it is more flexible and expressive.
+Performance: Performance: Gradle is generally faster, especially for large projects. This is because Gradle uses a dependency graph to determine which tasks need to be executed, while Maven uses a linear build order.
