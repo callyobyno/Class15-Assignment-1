dabc5be28082ac6908fceae9cfa72dc1603acaa3
Kelvinskell <kelvinskelll@gmail.com>
2023-03-12 05:47:46 +0100

    restructure files

--- Cally/assignment3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- If statment that compares the first command-line argument with the second command-line argument.
- Also check if the first line argument is greater than the second line argument.
- 
- If [[ $1 -gt $2 ]]
-
-then
